f723c6213fd31b9ed0860e2e983e72ee9580fc37
blasco <blascoburguillos@gmail.com>
2025-02-08 20:52:34 +0100

    Implement SimpleCalculator with Add method and unit tests

--- kata-2-string-calculator/dotnet/TDDKata.Tests/UnitTestKata2.cs
+++ kata-2-string-calculator/dotnet/TDDKata.Tests/UnitTestKata2.cs
@@ -1,15 +1,18 @@
-namespace TDDKata.Tests;
-
-public class Tests
+namespace TDDKata.Tests
 {
-    [SetUp]
-    public void Setup()
+    public class TestSimpleCalculator
     {
-    }
-
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+        [TestCase("", 0)]
+        [TestCase("3", 3)]
+        [TestCase("0, 1", 1)]
+        [TestCase("1, 2", 3)]
+        [TestCase("1, 2, 3, 4, 5", 15)]
+        [TestCase("1, 2 \n 5", 8)]
+        public void GivenAStringInput_ItReturnsTheExpectedResult(string input, int expected)
+        {
+            var calculator = new SimpleCalculator();
+            var result = calculator.Add(input);
+            Assert.That(result, Is.EqualTo(expected));
+        }
     }
 }
--- kata-2-string-calculator/dotnet/TDDKata/TDDKata2.cs
+++ kata-2-string-calculator/dotnet/TDDKata/TDDKata2.cs
@@ -1,6 +1,25 @@
 ﻿namespace TDDKata;
 
-public class Class1
+public class SimpleCalculator
 {
+    public int Add(string input)
+    {
+        bool success = GetNumbers(input, out var numbers);
 
+        if (numbers.Count() == 0)
+        {
+            return 0;
+        }
+
+        return numbers.Sum();
+    }
+
+    private static bool GetNumbers(string input, out IEnumerable<int> numbers)
+    {
+        numbers = input.Split([',', '\n'])
+                       .Select(number => int.TryParse(number.Trim(), out var result) ? (int?)result : null)
+                       .Where(n => n.HasValue)
+                       .Select(static n => n.GetValueOrDefault());
+        return true;
+    }
 }
